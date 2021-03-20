3be58f477577b74336fb2aeb1989f66005c898bd
Sirawit <popeye-zz@hotmail.com>
2021-03-20 23:24:39 +0700

    js test

---
+++ app.js
@@ -0,0 +1,21 @@
+alert('test script js')
+
+console.log('\"3\" === 4') 
+console.log('3' === 4) // check data and datatype 
+
+let x = '3' // string is ' ' or "
+let y = 'xxx'
+let z = 'zzz'
+let a='222', b='222';   // like a c language
+
+console.log(typeof x)
+
+newElement()
+
+function newElement(){
+
+    document.getElementById("heyyo").innerHTML = "nani the fuck"
+    console.log("heyyo nani the fuck")
+
+}
+
